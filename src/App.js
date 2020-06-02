c9a15f4597e6c622d012964e8e63c7b3380eb4ef
Brian Kozub <ticet11@gmail.com>
2020-06-02 11:35:32 -0600

    Basic functionality. Will display contact card with no styling.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import ContactCard from "./components/ContactCard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ContactCard
+                firstName="Brian"
+                lastName="Kozub"
+                phoneNumber="385-722-0126"
+            />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/ContactCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import phone from "../images/phone.png";
+
+export default function ContactCard(props) {
+    const firstName = props.firstName;
+    const lastName = props.lastName;
+    const phoneNumber = props.phoneNumber;
+    return (
+        <div>
+            <img style={{ width: "50px" }} src={phone} alt="" />
+            <p>{firstName}</p>
+            <p>{lastName}</p>
+            <p>{phoneNumber}</p>
+        </div>
+    );
+}
